7f351d76a1970f1a1a7d1f688eb8a76a2c3be7f1
monier34 <santerjov@gmail.com>
2022-08-07 18:28:34 +0300

    update

--- main.py
+++ main.py
@@ -1,20 +1,64 @@
+# Импорт библиотек
+import matplotlib.pyplot as plt
 import random
 
-dice_random = random.randint(1, 6)
-dice_random1 = random.randint(1, 6)
-dice_sum = dice_random + dice_random1
 
-player_choice = 1
+# Функция броска кубика
+def roll_dice():
+    dice_1 = random.randint(1, 6)
+    dice_2 = random.randint(1, 6)
 
-while player_choice != dice_sum:
-    player_choice = int(input("Введите число от 1 до 12: "))
-    if player_choice < dice_sum:
-        print("Компьютер выйграл! ")
-    elif player_choice > dice_sum:
-        print("Вы выйграли! ")
+    # Определяет является ли значение на костях одинаковым
+    if dice_1 == dice_2:
+        same_num = True
     else:
-        print("Сумма одиноковая. Ничья! ")
+        same_num = False
+    return same_num
 
-    play_again = input("Сыграем еще? (д/н): ")
-    if play_again.lower() != "д":
-        break
+
+# Входные данные
+num_silumations = 10000
+max_num_rolls = 1000
+bet = 1
+
+# Отслеживаемые переменные
+win_probability = []
+end_balance = []
+
+# Фигура, для симуляции баланса
+fig = plt.figure()
+plt.title("Monte Carlo Dice Game [" + str(num_silumations) + "simulation]")
+plt.xlabel("Roll Number")
+plt.ylabel("Balance [$]")
+plt.xlim([0, max_num_rolls])
+
+# Цикл for запускает желаемое количество симуляций
+for i in range(num_silumations):
+    balance = [1000]
+    num_rolls = [0]
+    num_wins = 0
+    # Выполняется до тех пор пока игрок не выкинет 1000 раз
+    while num_rolls[-1] < max_num_rolls:
+        same = roll_dice()
+        # Результат если кости одинаковые
+        if same:
+            balance.append(balance[-1] + 4 * bet)
+            num_wins += 1
+        # Результат если кости разные
+        else:
+            balance.append(balance[-1] - bet)
+
+        num_rolls.append(num_rolls[-1] + 1)
+
+    # Сохраняем отслеживаемую переменную и добавляем строку к рисунку
+    win_probability.append(num_wins/num_rolls[-1])
+    end_balance.append(balance[-1])
+    plt.plot(num_rolls, balance)
+
+plt.show()
+
+overall_win_probability = sum(win_probability)/len(win_probability)
+overall_end_balance = sum(end_balance)/len(end_balance)
+
+print("Average win propability after " +str(num_silumations) + "runs: " +str(overall_win_probability))
+print("Average ending balance after " +str(num_silumations) + "runs: $" +str(overall_end_balance))
